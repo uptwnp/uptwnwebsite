cc510789374345a202b38d4e087bcb402f790122
digiheadway <99395515+digiheadway@users.noreply.github.com>
2025-12-19 19:53:22 +0530

    i

---
+++ app/mylinks.tsx
@@ -0,0 +1,8 @@
+export default function LinksPage() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Quick Links hb</h1>
+      <a href="https://google.com">Poogle </a>
+    </div>
+  );
+}
